d9d47c04896d8345ffa2ddae2dc16c3546df5ca0
Stephen Booth <me@sbooth.org>
2025-11-13 11:25:33 -0600

    Make owner assertion functions inline (#1)

--- Sources/CXXOSUnfairLock/SFBOSUnfairLock.cpp
+++ Sources/CXXOSUnfairLock/SFBOSUnfairLock.cpp
@@ -3,18 +3,3 @@
 // Part of https://github.com/sbooth/CXXOSUnfairLock
 // MIT license
 //
-
-#import "SFBOSUnfairLock.hpp"
-
-// These methods could also be included in the header
-// but SPM doesn't support header-only libraries.
-
-void SFB::OSUnfairLock::assert_owner() noexcept
-{
-	os_unfair_lock_assert_owner(&lock_);
-}
-
-void SFB::OSUnfairLock::assert_not_owner() noexcept
-{
-	os_unfair_lock_assert_not_owner(&lock_);
-}
