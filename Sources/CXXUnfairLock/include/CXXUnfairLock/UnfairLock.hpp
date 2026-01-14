7eea1a6d259c8d844196b18aacc73abd3290e915
Stephen Booth <me@sbooth.org>
2026-01-14 15:08:53 -0600

    Rewrap

--- Sources/CXXUnfairLock/include/CXXUnfairLock/UnfairLock.hpp
+++ Sources/CXXUnfairLock/include/CXXUnfairLock/UnfairLock.hpp
@@ -70,8 +70,8 @@ public:
 	template <typename Func, typename... Args>
 	auto with_lock(Func&& func, Args&&... args) noexcept(std::is_nothrow_invocable_v<Func, Args...>);
 
-	/// Attempts to execute a callable within a locked scope if the lock can be
-	/// acquired immediately.
+	/// Attempts to execute a callable within a locked scope if the lock can
+	/// be acquired immediately.
 	///
 	/// Uses std::try_to_lock to attempt acquisition. If the lock is busy,
 	/// the function returns immediately without executing the callable.
