6a6c17a222e2260afe1b204c5b1e6ca77e90280c
Stephen Booth <me@sbooth.org>
2026-01-14 09:57:23 -0600

    Move method bodies outside of class declaration (#10)
    ## Pull request overview
    
    This pull request refactors the `UnfairLock` class by moving method
    implementations outside the class declaration. The methods are now
    declared inside the class body and defined below it using inline
    definitions.
    
    **Changes:**
    - Method declarations within the class now only contain signatures (no
    implementation bodies)
    - All method implementations moved to a separate "Implementation"
    section after the class declaration
    - API availability attribute moved from before the method signature to
    after for the `lock(os_unfair_lock_flags_t flags)` overload

--- Sources/CXXUnfairLock/include/CXXUnfairLock/UnfairLock.hpp
+++ Sources/CXXUnfairLock/include/CXXUnfairLock/UnfairLock.hpp
@@ -34,31 +34,18 @@ public:
 	// MARK: Lockable
 
 	/// Locks the lock.
-	void lock() noexcept __attribute__((acquire_capability()))
-	{
-		os_unfair_lock_lock(&lock_);
-	}
+	void lock() noexcept __attribute__((acquire_capability()));
 
 	/// Locks the lock.
 	/// @param flags Flags to alter the behavior of the lock.
-	API_AVAILABLE(macos(15.0), ios(18.0), tvos(18.0), watchos(11.0), visionos(2.0))
-	void lock(os_unfair_lock_flags_t flags) noexcept __attribute__((acquire_capability()))
-	{
-		os_unfair_lock_lock_with_flags(&lock_, flags);
-	}
+	void lock(os_unfair_lock_flags_t flags) noexcept __attribute__((acquire_capability())) API_AVAILABLE(macos(15.0), ios(18.0), tvos(18.0), watchos(11.0), visionos(2.0));
 
 	/// Unlocks the lock.
-	void unlock() noexcept __attribute__((release_capability()))
-	{
-		os_unfair_lock_unlock(&lock_);
-	}
+	void unlock() noexcept __attribute__((release_capability()));
 
 	/// Attempts to lock the lock.
 	/// @return true if the lock was successfully locked, false if the lock was already locked.
-	[[nodiscard]] bool try_lock() noexcept __attribute__((try_acquire_capability(true)))
-	{
-		return os_unfair_lock_trylock(&lock_);
-	}
+	[[nodiscard]] bool try_lock() noexcept __attribute__((try_acquire_capability(true)));
 
 	// MARK: Ownership
 
@@ -68,10 +55,7 @@ public:
 	///
 	/// If the lock is unlocked or owned by a different thread, this function
 	/// asserts and terminates the process.
-	void assert_owner() const noexcept
-	{
-		os_unfair_lock_assert_owner(&lock_);
-	}
+	void assert_owner() const noexcept;
 
 	///	Asserts that the calling thread is not the current owner of the lock.
 	///
@@ -79,14 +63,47 @@ public:
 	///
 	///	If the lock is currently owned by the current thread, this function asserts
 	///	and terminates the process.
-	void assert_not_owner() const noexcept
-	{
-		os_unfair_lock_assert_not_owner(&lock_);
-	}
+	void assert_not_owner() const noexcept;
 
 private:
 	/// The primitive lock.
 	os_unfair_lock lock_{OS_UNFAIR_LOCK_INIT};
 };
+
+// MARK: - Implementation -
+
+// MARK: Lockable
+
+inline void UnfairLock::lock() noexcept
+{
+	os_unfair_lock_lock(&lock_);
+}
+
+inline void UnfairLock::lock(os_unfair_lock_flags_t flags) noexcept
+{
+	os_unfair_lock_lock_with_flags(&lock_, flags);
+}
+
+inline void UnfairLock::unlock() noexcept
+{
+	os_unfair_lock_unlock(&lock_);
+}
+
+inline bool UnfairLock::try_lock() noexcept
+{
+	return os_unfair_lock_trylock(&lock_);
+}
+
+// MARK: Ownership
+
+inline void UnfairLock::assert_owner() const noexcept
+{
+	os_unfair_lock_assert_owner(&lock_);
+}
+
+inline void UnfairLock::assert_not_owner() const noexcept
+{
+	os_unfair_lock_assert_not_owner(&lock_);
+}
 
 } /* namespace CXXUnfairLock */
